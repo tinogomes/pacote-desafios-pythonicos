7ee12a00c9ce5c6af7fa1ce1eedc041a15aaecab
tinogomes <tinorj@gmail.com>
2020-11-10 02:10:45 -0300

    correção para cem mil

--- 15_number_in_cardinal.py
+++ 15_number_in_cardinal.py
@@ -46,15 +46,12 @@ def number_in_cardinal(number, nindex=0):
         value = value_base = number - calculated
         if not value: break
 
-        try:
-            result += NUMBERS[value_base][nindex]
-            break
-        except KeyError:
+        if value > 19:
             value //= divisor
             if value_base < THOUSAND: value *= divisor
             if not value: continue
 
-        result += number_in_cardinal(value, -1)
+        result += number_in_cardinal(value, -1 if (divisor == value) else 0)
         _suffix = suffix[0] if value == 1 else suffix[-1]
         if _suffix: result += ' ' + _suffix
 
@@ -103,11 +100,18 @@ if __name__ == '__main__':
     test(number_in_cardinal, 1011, "um mil e onze")
     test(number_in_cardinal, 1234, "um mil e duzentos e trinta e quatro")
     test(number_in_cardinal, 12345, "doze mil e trezentos e quarenta e cinco")
+    test(number_in_cardinal, 100000, "cem mil")
     test(number_in_cardinal, 123456, "cento e vinte e três mil e quatrocentos e cinquenta e seis")
     test(number_in_cardinal, 1234567, "um milhão e duzentos e trinta e quatro mil e quinhentos e sessenta e sete")
     test(number_in_cardinal, 12345678, "doze milhões e trezentos e quarenta e cinco mil e seiscentos e setenta e oito")
+    test(number_in_cardinal, 100000000, "cem milhões")
     test(number_in_cardinal, 123456789, "cento e vinte e três milhões e quatrocentos e cinquenta e seis mil e setecentos e oitenta e nove")
     test(number_in_cardinal, 1234567890, "um bilhão e duzentos e trinta e quatro milhões e quinhentos e sessenta e sete mil e oitocentos e noventa")
     test(number_in_cardinal, 21345678901, "vinte e um bilhões e trezentos e quarenta e cinco milhões e seiscentos e setenta e oito mil e novecentos e um")
+    test(number_in_cardinal, 100000000000, "cem bilhões")
     test(number_in_cardinal, 1000000000000, "um trilhão")
     test(number_in_cardinal, 2000000000000, "dois trilhões")
+    test(number_in_cardinal, 100000000000000, "cem trilhões")
+    test(number_in_cardinal, 101000000000000, "cento e um trilhões")
+    test(number_in_cardinal, 110000000000000, "cento e dez trilhões")
+    test(number_in_cardinal, 111000000000000, "cento e onze trilhões")
